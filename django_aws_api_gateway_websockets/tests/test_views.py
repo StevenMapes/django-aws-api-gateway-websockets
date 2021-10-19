52bbe3d1d09a867b3d0d53dad1083e6613959231
Steve Mapes <steve@stevenmapes.com>
2021-10-19 17:14:22 +0100

    Adding in some initial tests

--- django_aws_api_gateway_websockets/tests/test_views.py
+++ django_aws_api_gateway_websockets/tests/test_views.py
@@ -1,16 +1,51 @@
+from unittest.mock import patch
+
+from django.http import HttpResponseBadRequest
+
 from django_aws_api_gateway_websockets import views
-from django.test import override_settings, TestCase
+from django.test import TestCase, SimpleTestCase, RequestFactory
 
 
-class WebSocketViewTestCase(TestCase):
+class WebSocketViewSimpleTestCase(SimpleTestCase):
+    """For tests the do not require a DB"""
 
-    def test_simple(self):
-        self.assertTrue(True)
+    def setUp(self) -> None:
+        self.factory = RequestFactory()
 
-    def test_as_view(self):
-        try:
-            views.WebSocketView.as_view()
-        except Exception as e:
-            print(e)
-            raise e
+    def test_default_route_selection_key(self):
+        self.assertEqual('action', views.WebSocketView.route_selection_key)
 
+    def test_default_required_headers(self):
+        self.assertEqual(
+            [
+                'Host', 'X-Real-Ip', 'X-Forwarded-For', 'X-Forwarded-Proto', 'Connection', 'Content-Length',
+                'X-Forwarded-Port', 'X-Amzn-Trace-Id', 'Connectionid', 'User-Agent', 'X-Amzn-Apigateway-Api-Id'
+            ],
+            views.WebSocketView.required_headers
+        )
+
+    def test_default_required_connection_headers(self):
+        self.assertEqual(
+            [
+                'Cookie', 'Origin', 'Sec-Websocket-Extensions', 'Sec-Websocket-Key', 'Sec-Websocket-Version'
+            ],
+            views.WebSocketView.required_connection_headers
+        )
+
+    def test_default_expected_useragent_prefix(self):
+        self.assertEqual('AmazonAPIGateway_', views.WebSocketView.expected_useragent_prefix)
+
+    def test_default_aws_api_gateway_id(self):
+        self.assertIsNone(views.WebSocketView.aws_api_gateway_id)
+
+    @patch('django_aws_api_gateway_websockets.views.logger')
+    def test__return_bad_request(self, logger):
+        """Should log a warning message and return a HttpBadRequest object"""
+        msg = 'This is the reason for the bad request'
+        request = self.factory.get('')
+        obj = views.WebSocketView()
+        res = obj._return_bad_request(request, msg)
+
+        logger.warning.assert_called_with(msg, extra={'status_code': 400, 'request': request})
+        self.assertIsInstance(obj, HttpResponseBadRequest)
+        self.assertEqual(400, res.status_code)
