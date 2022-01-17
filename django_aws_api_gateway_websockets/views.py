49a015eed4fc71839676fc513472e1db344c6d29
Steve Mapes <steve@stevenmapes.com>
2022-01-17 23:07:49 +0000

    Fixing the user saving

--- django_aws_api_gateway_websockets/views.py
+++ django_aws_api_gateway_websockets/views.py
@@ -179,7 +179,7 @@ class WebSocketView(View):
         WebSocketSession.objects.create(
             connection_id=request.headers["Connectionid"],
             channel=request.GET.get("channel", ""),
-            user=request.user if request.user_id > 0 else None,
+            user=request.user if request.user.is_authenticated else None,
         )
 
         return JsonResponse({})
