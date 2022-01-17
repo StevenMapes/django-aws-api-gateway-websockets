c2c03229450e380c614b312a12e74900da1dd458
Steve Mapes <steve@stevenmapes.com>
2022-01-17 22:05:53 +0000

    Updating the requirements

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 0.0.2 - 17th Jan 2022
+HOTFIX - Dropped support for Django < 3.1 due to use of PositiveBigInt and also taking the design choice to drop 
+support of Python <3.8 
+
+# 0.0.1 - 17th Jan 2022
+Initial build with the base view and base model.
+Should Supports Python 3.6+ and Django 2.2+
--- requirements/compile.py
+++ requirements/compile.py
@@ -19,44 +19,6 @@ if __name__ == "__main__":
             "python",
             *common_args,
             "-P",
-            "Django>=2.2,<2.3",
-            "-P",
-            "typing-extensions==3.10.0.2",
-            "-o",
-            "py36-django22.txt",
-        ],
-        check=True,
-        capture_output=True,
-    )
-    subprocess.run(
-        [
-            "python",
-            *common_args,
-            "-P",
-            "Django>=2.2,<2.3",
-            "-o",
-            "py38-django22.txt",
-        ],
-        check=True,
-        capture_output=True,
-    )
-    subprocess.run(
-        [
-            "python",
-            *common_args,
-            "-P",
-            "Django>=3.0a1,<3.1",
-            "-o",
-            "py38-django30.txt",
-        ],
-        check=True,
-        capture_output=True,
-    )
-    subprocess.run(
-        [
-            "python",
-            *common_args,
-            "-P",
             "Django>=3.1a1,<3.2",
             "-o",
             "py38-django31.txt",
@@ -84,42 +46,6 @@ if __name__ == "__main__":
             "Django>=4.0a1,<4.1",
             "-o",
             "py38-django40.txt",
-        ],
-        check=True,
-        capture_output=True,
-    )
-    subprocess.run(
-        [
-            "python",
-            *common_args,
-            "-P",
-            "Django>=2.2,<2.3",
-            "-o",
-            "py39-django22.txt",
-        ],
-        check=True,
-        capture_output=True,
-    )
-    subprocess.run(
-        [
-            "python",
-            *common_args,
-            "-P",
-            "Django>=3.0a1,<3.1",
-            "-o",
-            "py39-django30.txt",
-        ],
-        check=True,
-        capture_output=True,
-    )
-    subprocess.run(
-        [
-            "python",
-            *common_args,
-            "-P",
-            "Django>=3.1a1,<3.2",
-            "-o",
-            "py39-django31.txt",
         ],
         check=True,
         capture_output=True,
@@ -162,9 +88,17 @@ if __name__ == "__main__":
     )
 
     # Use SED to remove the --extra-index-url lines from every file
-    sed_args = ["sed", "-i", "-e", 's/--extra-index-url .*$//g']
-    [subprocess.run([*sed_args, x.name]) for x in Path('.').iterdir() if not x.is_dir() and '.txt' in x.name]
+    sed_args = ["sed", "-i", "-e", "s/--extra-index-url .*$//g"]
+    [
+        subprocess.run([*sed_args, x.name])
+        for x in Path(".").iterdir()
+        if not x.is_dir() and ".txt" in x.name
+    ]
 
     # Remove backports from Python3.10
-    sed_args = ["sed", "-i", "-e", '/backports/,/via django/d']
-    [subprocess.run([*sed_args, x.name]) for x in Path('.').iterdir() if not x.is_dir() and 'py310-' in x.name]
+    sed_args = ["sed", "-i", "-e", "/backports/,/via django/d"]
+    [
+        subprocess.run([*sed_args, x.name])
+        for x in Path(".").iterdir()
+        if not x.is_dir() and "py310-" in x.name
+    ]
